d606064da40c58a060632613a4def8e0a5201136
Stefan Liu <bill72614321@gmail.com>
2023-12-09 10:22:24 -0800

    Adding icons

---
+++ GHFollowerSwiftUI/GHFollowerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowerSwiftUIApp.swift
+//  GHFollowerSwiftUI
+//
+//  Created by 劉力源 on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GHFollowerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
